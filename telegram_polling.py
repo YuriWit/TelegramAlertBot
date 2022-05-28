ca04affd1d92e1fd722a45bdf49fcccb07d558fe
YuriWit <73124184+YuriWit@users.noreply.github.com>
2022-05-27 21:39:41 -0300

    added space

--- telegram_polling.py
+++ telegram_polling.py
@@ -4,7 +4,7 @@ bot = telebot.TeleBot("5382490304:AAHAVgrcmrKFoSx2pNrjVpsAYF8aeQlz-Bc")
 
 @bot.message_handler(commands=['getid'])
 def send_welcome(message):
-    bot.reply_to(message, "chat id:" + str(message.chat.id))
+    bot.reply_to(message, "chat id: " + str(message.chat.id))
 
 @bot.message_handler(commands=['help'])
 def send_welcome(message):
