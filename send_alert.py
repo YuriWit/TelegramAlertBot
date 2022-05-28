97858b7bf30dc27cb8e12dbeaf5ff2c3e73e94d3
Yuri de Wit Herran <yuri@MacBook-Air-de-Yuri.local>
2022-05-28 11:22:47 -0300

    added example for get

--- send_alert.py
+++ send_alert.py
@@ -2,8 +2,13 @@ import requests, json
 
 url = "http://52.70.34.69/alert/2113705847"
 
+
+# post
 data = {"alert": "this is the message"}
 json_data = json.dumps(data)
-
 r = requests.post(url, json=json_data)
+print(r.text)
+
+# get
+r = requests.get(url + "/?alert=nospacemessage")
 print(r.text)
