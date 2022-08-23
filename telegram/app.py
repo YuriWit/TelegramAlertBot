290feb367c0e38f51eaff7b3fbec9406c8958198
YuriWit <yuridewitherran@gmail.com>
2022-08-23 17:32:56 -0300

    added get token method

--- telegram/app.py
+++ telegram/app.py
@@ -8,6 +8,14 @@ def newToken(size):
     random.seed(time.time())
     return ''.join(random.choices(string.ascii_lowercase + string.ascii_uppercase + string.digits, k = size))
 
+@bot.message_handler(commands=['token'])
+def send_token(message):
+    if message.chat.id not in tokens:
+        bot.reply_to(message, 'no active token')
+        return
+    bot.reply_to(message, 'token:')
+    bot.send_message(message.chat.id, tokens[message.chat.id])
+
 @bot.message_handler(commands=['newToken'])
 def send_new_token(message):
     token = newToken(12)
@@ -26,8 +34,11 @@ def send_welcome(message):
 @bot.message_handler(commands=['help'])
 def send_welcome(message):
 	bot.reply_to(message,'''
-/newToken - to get your chat token
-/seen - confirm recived''')
+/help
+/token
+/newToken
+/seen
+''')
 
 
 if __name__ == '__main__':
